9f1ba65e76dfda7c4878f1ce57aa501910f7717f
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-23 14:51:38 -0400

    jacoco functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,17 +6,15 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
+    jacoco
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // Use JUnit test framework.
     testImplementation("junit:junit:4.13.2")
 
     // This dependency is used by the application.
@@ -33,4 +31,22 @@ java {
 application {
     // Define the main class for the application.
     mainClass.set("cpuscheduling.App")
+}
+
+/* Test Configurations */
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        csv.required.set(true)
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.10"
 }
