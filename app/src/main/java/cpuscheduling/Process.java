9d1874d77d0a7fe518d4e3058b3f938a2cc7c683
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-01 23:20:11 -0400

    create process class

---
+++ app/src/main/java/cpuscheduling/Process.java
@@ -0,0 +1,15 @@
+package cpuscheduling;
+
+/**
+ * Process class to store a process' data.
+ */
+public class Process {
+
+    /**
+     * Constructor for the Process class.
+     */
+    public Process() {
+        // Set process ID number
+        // Increment process ID number for the next object
+    }
+}
---
+++ app/src/test/java/cpuscheduling/ProcessTest.java
@@ -0,0 +1,12 @@
+package cpuscheduling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProcessTest {
+  @Test
+  public void testProcessID() {
+    assert false;
+  }
+}
