c270b46d9af0f805c737349168ecdebfed28eff1
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-02 13:19:05 -0400

    create burst time variable

--- app/src/main/java/cpuscheduling/Process.java
+++ app/src/main/java/cpuscheduling/Process.java
@@ -1,15 +1,35 @@
 package cpuscheduling;
 
-/**
- * Process class to store a process' data.
- */
+/** Process class to store a process' data. */
 public class Process {
 
-    /**
-     * Constructor for the Process class.
-     */
-    public Process() {
-        // Set process ID number
-        // Increment process ID number for the next object
-    }
+  /** The CPU Burst time required of this Process. */
+  private int burstTime;
+
+  /**
+   * Constructor for the Process class.
+   *
+   * @param bTime the burst time required for this Process.
+   */
+  public Process(int bTime) {
+    setBurstTime(bTime);
+  }
+
+  /**
+   * Get this Process' CPU burst time.
+   *
+   * @return the burst time.
+   */
+  public int getBurstTime() {
+    return this.burstTime;
+  }
+
+  /**
+   * Set this Process' CPU burst time.
+   *
+   * @param time required CPU burst time.
+   */
+  public void setBurstTime(int time) {
+    this.burstTime = time;
+  }
 }
--- app/src/test/java/cpuscheduling/ProcessTest.java
+++ app/src/test/java/cpuscheduling/ProcessTest.java
@@ -2,11 +2,30 @@ package cpuscheduling;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ProcessTest {
+  static Process p1, p2;
+
+  @BeforeClass
+  public static void setUp() {
+    p1 = new Process(5);
+    p2 = new Process(12);
+  }
+
   @Test
-  public void testProcessID() {
-    assert false;
+  public void testBurstTime() {
+    assert p1.getBurstTime() == 5;
+    assert p2.getBurstTime() == 12;
+
+    Process p3 = new Process(63);
+    assert p3.getBurstTime() == 63;
+
+    p1.setBurstTime(55);
+    assert p1.getBurstTime() == 55;
+
+    p2.setBurstTime(3);
+    assert p2.getBurstTime() == 3;
   }
 }
