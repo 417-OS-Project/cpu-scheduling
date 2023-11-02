40f016542768f9b98d9201d76a8806075d6deafb
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-02 14:30:21 -0400

    create priority variable

--- app/src/main/java/cpuscheduling/Process.java
+++ app/src/main/java/cpuscheduling/Process.java
@@ -1,15 +1,38 @@
 package cpuscheduling;
 
-/**
- * Process class to store a process' data.
- */
+/** Process class to store a process' data. */
 public class Process {
 
-    /**
-     * Constructor for the Process class.
-     */
-    public Process() {
-        // Set process ID number
-        // Increment process ID number for the next object
+  /** The priority of this Process. */
+  private int priority;
+
+  /**
+   * Constructor for the Process class.
+   *
+   * @param prior the priority of this Process.
+   */
+  public Process(int prior) {
+    setPriority(prior);
+  }
+
+  /**
+   * Get the priority of this Process.
+   *
+   * @return priority of this Process.
+   */
+  public int getPriority() {
+    return this.priority;
+  }
+
+  /**
+   * Set the priority of this Process.
+   *
+   * @param prior the priority integer.
+   */
+  public void setPriority(int prior) {
+    if (prior < 0) {
+      prior = prior * -1;
     }
+    this.priority = prior;
+  }
 }
--- app/src/test/java/cpuscheduling/ProcessTest.java
+++ app/src/test/java/cpuscheduling/ProcessTest.java
@@ -2,11 +2,24 @@ package cpuscheduling;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ProcessTest {
+  static Process p1, p2;
+
+  @BeforeClass
+  public static void setUp() {
+    p1 = new Process(5);
+    p2 = new Process(2);
+  }
+
   @Test
-  public void testProcessID() {
-    assert false;
+  public void testPriority() {
+    assertEquals(5, p1.getPriority());
+    assertEquals(2, p2.getPriority());
+
+    p1.setPriority(-3);
+    assertEquals(3, p1.getPriority());
   }
 }
