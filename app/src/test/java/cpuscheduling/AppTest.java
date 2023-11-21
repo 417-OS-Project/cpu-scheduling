767f025df0c098fafb9802e79f286a5fa6de76b6
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-20 19:59:49 -0500

    restructure file parsing

--- app/src/test/java/cpuscheduling/AppTest.java
+++ app/src/test/java/cpuscheduling/AppTest.java
@@ -2,6 +2,8 @@ package cpuscheduling;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,5 +14,14 @@ public class AppTest {
     assertNull(App.parseLine("Arrival Burst Priority"));
     Assert.assertArrayEquals(App.parseLine("148\t28\t10"), new int[] {148, 28, 10});
     Assert.assertArrayEquals(App.parseLine("148 28 10"), new int[] {148, 28, 10});
+  }
+
+  @Test
+  public void testParseFile() {
+    ArrayList<Process> collection = App.parseFile(new File("not a real file . fake"));
+    assertTrue(collection.isEmpty());
+
+    collection = App.parseFile(new File("src/resources/SmallDataFile.txt"));
+    assertEquals(10, collection.size());
   }
 }
