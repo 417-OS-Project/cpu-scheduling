e3d83862ba7dd1c906ab72c8bbf2a46a457a5f9a
TonyGrif <TonyGriffin2000@gmail.com>
2023-11-22 20:12:11 -0500

    incorrect, default cycle behavior

--- app/src/main/java/cpuscheduling/PriorityScheduler.java
+++ app/src/main/java/cpuscheduling/PriorityScheduler.java
@@ -1,49 +1,128 @@
 package cpuscheduling;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * Priority (with Preemption) scheduler.
- */
+/** Priority (with Preemption) scheduler. */
 public class PriorityScheduler {
-    /**
-     * Queue of Processes.
-     */
-    private Queue<Process> queue;
+  /** Queue of Processes. */
+  private Queue<Process> queue;
 
-    /**
-     * Stat tracker for this scheduler.
-     */
-    StatTracker stats;
+  /** The process currently using the CPU. */
+  Process currentProcess;
 
-    /**
-     * Constructor for the Priority scheduler.
-     */
-    public PriorityScheduler() {
-        this.queue = new LinkedList<>();
-        stats = new StatTracker();
+  /** List of available processes. */
+  private ArrayList<Process> list;
+
+  /** Stat tracker for this scheduler. */
+  StatTracker stats;
+
+  /** Constructor for the Priority scheduler. */
+  public PriorityScheduler() {
+    this.queue = new LinkedList<>();
+    this.currentProcess = null;
+    this.list = new ArrayList<>();
+    this.stats = new StatTracker();
+  }
+
+  /** Run the scheduler until it cannot anymore. */
+  public void fullCycle() {
+    while (this.canContinue()) {
+      this.cycle();
+    }
+  }
+
+  /** Conduct one cycle of this scheduler. */
+  public void cycle() {
+    updateList();
+    if (this.currentProcess == null) {
+      if (!this.list.isEmpty()) {
+        this.currentProcess = this.list.remove(0);
+      } else {
+        // The null case
+        stats.updateStats(null);
+        return;
+      }
     }
 
-    /**
-     * Add a new process to the queue.
-     *
-     * @param newProcess process to be added.
-     */
-    public void addProcess(Process newProcess) {
-        this.queue.add(newProcess);
+    // No more cycles for current process
+    if (this.currentProcess.getRemainingBurstTime() == 0) {
+      this.currentProcess = null;
+      if (!this.list.isEmpty()) {
+        this.currentProcess = this.list.remove(0);
+      } else if (!canContinue()) {
+        // Nothing left to do
+        return;
+      } else {
+        stats.updateStats(null);
+        return;
+      }
     }
 
-    /**
-     * Return the size of the current queue.
-     *
-     * @return size of queue.
-     */
-    public int getSizeOfQueue() {
-        return this.queue.size();
-    }
+    this.currentProcess.decrementRemainingBurst();
+    stats.updateStats(this.currentProcess);
+  }
 
-    public int getTotalProcessCount() {
-        return stats.getTotalProcessCount();
+  /** Update the list when a new Process arrives. */
+  private void updateList() {
+    if (!this.queue.isEmpty()
+        && this.queue.peek().getArrivalTime() <= this.stats.getTotalElapsedTime()) {
+      this.list.add(this.queue.remove());
     }
+  }
+
+  /**
+   * Add a new process to the queue.
+   *
+   * @param newProcess process to be added.
+   */
+  public void addProcess(Process newProcess) {
+    this.queue.add(newProcess);
+  }
+
+  /**
+   * Return the size of the current queue.
+   *
+   * @return size of queue.
+   */
+  public int getSizeOfQueue() {
+    return this.queue.size();
+  }
+
+  /**
+   * Return the burst time remaining on the current process.
+   *
+   * @return remaining burst time.
+   */
+  public int getCurrentBurstRemaining() {
+    return this.currentProcess.getRemainingBurstTime();
+  }
+
+  /**
+   * Return the total number of processes that have accessed the CPU.
+   *
+   * @return total number of processes.
+   */
+  public int getTotalProcessCount() {
+    return stats.getTotalProcessCount();
+  }
+
+  /**
+   * Return the total elapsed time of this scheduler.
+   *
+   * @return total elapsed time.
+   */
+  public int getTotalElapsedTime() {
+    return stats.getTotalElapsedTime();
+  }
+
+  /**
+   * Utility function to determine if the scheduler can continue.
+   *
+   * @return true if there are more processes, false if not.
+   */
+  public Boolean canContinue() {
+    return this.currentProcess != null || !this.queue.isEmpty() || !this.list.isEmpty();
+  }
 }
--- app/src/test/java/cpuscheduling/PriorityTest.java
+++ app/src/test/java/cpuscheduling/PriorityTest.java
@@ -2,34 +2,52 @@ package cpuscheduling;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-
 public class PriorityTest {
-    PriorityScheduler pFull;
+  PriorityScheduler pFull;
 
-    @Before
-    public void init() {
-        pFull = new PriorityScheduler();
-        ArrayList<Process> parsed;
-        parsed = App.parseFile(new File("src/resources/SmallDataFile.txt"));
+  @Before
+  public void init() {
+    pFull = new PriorityScheduler();
+    ArrayList<Process> parsed;
+    parsed = App.parseFile(new File("src/resources/SmallDataFile.txt"));
 
-        while (!parsed.isEmpty()) {
-            pFull.addProcess(parsed.get(0));
-            parsed.remove(0);
-        }
+    while (!parsed.isEmpty()) {
+      pFull.addProcess(parsed.get(0));
+      parsed.remove(0);
     }
+  }
 
-    @Test
-    public void testAddProcess() {
-        assertEquals(10, pFull.getSizeOfQueue());
-        assertEquals(0, pFull.getTotalProcessCount());
+  @Test
+  public void testAddProcess() {
+    assertEquals(10, pFull.getSizeOfQueue());
+    assertEquals(0, pFull.getTotalProcessCount());
 
-        pFull.addProcess(new Process(1, 2, 3));
-        assertEquals(11, pFull.getSizeOfQueue());
-        assertEquals(0, pFull.getTotalProcessCount());
+    pFull.addProcess(new Process(1, 2, 3));
+    assertEquals(11, pFull.getSizeOfQueue());
+    assertEquals(0, pFull.getTotalProcessCount());
+  }
+
+  @Test
+  public void testStats() {
+    assertEquals(0, pFull.getTotalProcessCount());
+    assertEquals(0, pFull.getTotalElapsedTime());
+
+    pFull.cycle();
+    assertEquals(0, pFull.getTotalProcessCount());
+    assertEquals(1, pFull.getTotalElapsedTime());
+
+    for (int i = 0; i < 10; i++) {
+      pFull.cycle();
     }
+    assertEquals(1, pFull.getTotalProcessCount());
+    assertEquals(11, pFull.getTotalElapsedTime());
+    assertEquals(21, pFull.getCurrentBurstRemaining());
+
+    pFull.fullCycle();
+  }
 }
